17794e45b93cb93f8197a497312d10c9a2a963b5
Jean Augusto <jean.aug02@gmail.com>
2019-11-28 16:45:23 -0300

    Criando primeira view

--- crud/app/Http/Controllers/autoresController.php
+++ crud/app/Http/Controllers/autoresController.php
@@ -9,10 +9,39 @@ class autoresController extends Controller
 {
     public function index(){
         $autores = autores::all();
-        $total = autores::all()->count();
-        return view('list-autores', compact('produtos','total'));
+        return view('list-autores', compact('autores'));
     }
     public function create(){
-        return view('include-produto')
+        return view('include-autores');
     }
+    public function store(Request $request){
+        $autores = new autores;
+        $autores->nome=$request->nome;
+        $autores->ano_nascimento=$request->ano_nascimento;
+        $autores->nacionalidade=$request->nacionalidade;
+        $autores->sexo=$request->sexo;
+        $autores->save();
+        return redirect()->route('autores.index')->with('message','Autor criado com sucesso!');
+    }
+    public function show($id){
+        //
+    }
+    public function edit($id){
+        $autores = autores::findOrFail($id);
+        return view('alter-autores',compact('autores'));
+    }
+    public function update(Request $request, $id){
+        $autores= autores::finfOrFail($id);
+        $autores-> nome= $request->nome;
+        $autores-> ano_nascimento= $request->ano_nascimento;
+        $autores-> nacionalidade= $request->nacionalidade;
+        $autores-> sexo= $request->sexo;
+        $autores->save();
+        return redirect()->repoute('autores.index')->with('message','Autor auterado com sucesso!');
+    }
+    public function destroy($id){
+        $autores = autores::FindeOrFail($id);
+        $autores->delete();
+        return redirect()->route('autores.index')->with('message','Autor excluido com sucesso!');
+        }
 }
